2f4ffbae2b658047c47e698acfb8c20023ed794d
zyrenrizvale <zyrenrizvale@gmail.com>
2026-08-06 22:58:50 +0700

    Add files via upload

--- app/src/main/java/com/rizki/targetku/ui/screens/AiTutorScreen.kt
+++ app/src/main/java/com/rizki/targetku/ui/screens/AiTutorScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
